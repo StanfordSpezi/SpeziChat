382fd879e7ccf083404b256152e8e2eadc899d0d
Philipp Zagar <zagar@stanford.edu>
2024-01-19 21:03:39 -0800

    Make UI tests of Export functionality more stable (#8)

--- Tests/UITests/TestAppUITests/TestAppUITests.swift
+++ Tests/UITests/TestAppUITests/TestAppUITests.swift
@@ -38,47 +38,57 @@ class TestAppUITests: XCTestCase {
     
     func testChatExport() throws {
         let app = XCUIApplication()
-        app.launch()
-        
-        // Entering dummy chat value
-        XCTAssert(app.staticTexts["SpeziChat"].waitForExistence(timeout: 1))
-        try app.textViews["Message Input Textfield"].enter(value: "User Message!", dismissKeyboard: false)
-        XCTAssert(app.buttons["Send Message"].waitForExistence(timeout: 5))
-        app.buttons["Send Message"].tap()
-        
-        sleep(1)
-        XCTAssert(app.staticTexts["Assistant Message Response!"].waitForExistence(timeout: 5))
-        
-        // Export chat via share sheet button
-        XCTAssert(app.buttons["Export the Chat"].waitForExistence(timeout: 2))
-        app.buttons["Export the Chat"].tap()
-
-        // Store exported chat in Files
-        XCTAssert(app.staticTexts["Save to Files"].waitForExistence(timeout: 10))
-        app.staticTexts["Save to Files"].tap()
-        sleep(3)
-        XCTAssert(app.buttons["Save"].waitForExistence(timeout: 2))
-        app.buttons["Save"].tap()
-        sleep(10)    // Wait until file is saved
-        
-        if app.staticTexts["Replace Existing Items?"].waitForExistence(timeout: 5) {
-            XCTAssert(app.buttons["Replace"].waitForExistence(timeout: 2))
-            app.buttons["Replace"].tap()
-            sleep(3)    // Wait until file is saved
-        }
-        
-        // Wait until share sheet closed and back on the chat screen
-        XCTAssert(app.staticTexts["SpeziChat"].waitForExistence(timeout: 10))
-        
-        XCUIDevice.shared.press(.home)
-        
-        // Launch the Files app
         let filesApp = XCUIApplication(bundleIdentifier: "com.apple.DocumentsApp")
-        filesApp.launch()
+        let maxRetries = 10
         
-        // Handle already open files
-        if filesApp.buttons["Done"].waitForExistence(timeout: 2) {
-            filesApp.buttons["Done"].tap()
+        for _ in 0...maxRetries {
+            app.launch()
+            
+            // Entering dummy chat value
+            XCTAssert(app.staticTexts["SpeziChat"].waitForExistence(timeout: 1))
+            try app.textViews["Message Input Textfield"].enter(value: "User Message!", dismissKeyboard: false)
+            XCTAssert(app.buttons["Send Message"].waitForExistence(timeout: 5))
+            app.buttons["Send Message"].tap()
+            
+            sleep(1)
+            XCTAssert(app.staticTexts["Assistant Message Response!"].waitForExistence(timeout: 5))
+            
+            // Export chat via share sheet button
+            XCTAssert(app.buttons["Export the Chat"].waitForExistence(timeout: 2))
+            app.buttons["Export the Chat"].tap()
+            
+            // Store exported chat in Files
+            XCTAssert(app.staticTexts["Save to Files"].waitForExistence(timeout: 10))
+            app.staticTexts["Save to Files"].tap()
+            sleep(3)
+            XCTAssert(app.buttons["Save"].waitForExistence(timeout: 2))
+            app.buttons["Save"].tap()
+            sleep(10)    // Wait until file is saved
+            
+            if app.staticTexts["Replace Existing Items?"].waitForExistence(timeout: 5) {
+                XCTAssert(app.buttons["Replace"].waitForExistence(timeout: 2))
+                app.buttons["Replace"].tap()
+                sleep(3)    // Wait until file is saved
+            }
+            
+            // Wait until share sheet closed and back on the chat screen
+            XCTAssert(app.staticTexts["SpeziChat"].waitForExistence(timeout: 10))
+            
+            XCUIDevice.shared.press(.home)
+            
+            // Launch the Files app
+            filesApp.launch()
+            
+            // Handle already open files
+            if filesApp.buttons["Done"].waitForExistence(timeout: 2) {
+                filesApp.buttons["Done"].tap()
+            }
+            
+            // Check if file exists - If not, try the export procedure again
+            // Saving to files is very flakey on the runners, needs multiple attempts to succeed
+            if filesApp.staticTexts["Exported Chat"].waitForExistence(timeout: 2) {
+                break
+            }
         }
         
         // Open File
