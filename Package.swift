8c77bc3a14befb1fe5ba43183758041ec0341025
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-10-11 23:20:53 +0200

    Allow dependencies to use Spezi Foundation 2.0

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .library(name: "SpeziChat", targets: ["SpeziChat"])
     ],
     dependencies: [
-        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation", from: "1.0.3"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation", from: "2.0.0-beta.3"),
         .package(url: "https://github.com/StanfordSpezi/SpeziSpeech", from: "1.0.1"),
         .package(url: "https://github.com/StanfordSpezi/SpeziViews", from: "1.3.1")
     ],
